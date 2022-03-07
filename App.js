4444420301a1f72379be724efb896421cac6dad8
dhwani2106 <99235884+dhwani2106@users.noreply.github.com>
2022-03-07 16:19:53 +0530

    textChange

--- App.js
+++ App.js
@@ -5,6 +5,13 @@
  * @format
  * @flow strict-local
  */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
 import React from 'react';
 import type {Node} from 'react';
